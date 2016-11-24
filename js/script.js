1236e2f7e1a9bb66fe3fca4347f8e60d8009c2eb
Djordje Vujatovic <djordje_v@hotmail.com>
2016-11-24 10:01:24 -0800

    Fixed header problem. Font problem in headings.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 //SMOOTH SCROLL////////////////////////////////////////
 
 $(function() {
+  var offset = $("header").height();
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - offset
         }, 1000);
         return false;
       }
